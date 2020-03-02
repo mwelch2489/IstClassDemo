2a0a114288fb152d94108f3dc91075a640b998e6
Matt Welch <mwelch2489@gmail.com>
2020-03-01 22:21:28 -0500

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { TopbarComponent } from './topbar/topbar.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -7,16 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  firstName: any;
-  lastName: any;
+  newCustomer = {
+  firstNameInput: '',
+  lastNameInput: '',
+  date: ''
+}
+firstname: any;
+lastname: any;
+date: any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onClickSubmit(){
 
+  onSubmit(){
+   this.firstname = "The user's first name is " + this.newCustomer.firstNameInput;
+   this.lastname = "The user's last name is " + this.newCustomer.lastNameInput;
+   this.date = "The user chose this date: " + this.newCustomer.date;
   }
+
+  onClear(){
+    this.newCustomer.firstNameInput = '';
+    this.newCustomer.lastNameInput = '';
+    this.newCustomer.date = '';
+    this.firstname = '';
+    this.lastname = '';
+    this.date = '';
+  }
+
 
 }
